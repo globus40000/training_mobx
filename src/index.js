22ac33b18be3610a45bc29e35bdaef9552fd838d
Pavel Ashifin <globus40000@gmail.com>
2018-12-12 09:06:47 +0600

    Basic Counter implementation with mobx

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 
-ReactDOM.render('Hello World', document.getElementById('root'));
+@observer
+class Counter extends React.Component {
+
+  @observable count = 0;
+
+  render() {
+    return (
+      <div>
+        Counter: {this.count} <br/>
+        <button onClick={this.handleInc}>+</button>
+        <button onClick={this.handleDec}>-</button>
+      </div>
+    );
+  }
+
+  handleInc = () => {
+    this.count += 1;
+  };
+
+  handleDec = () => {
+    this.count -= 1;
+  };
+
+}
+
+ReactDOM.render(<Counter/>, document.getElementById('root'));
